d9f5cdcf69d52680765f5a1297c9633131b3eb0a
Renata Guerra <renatajarmova@gmail.com>
2021-03-26 19:22:57 +0000

    fix(Card): Add new asset for video thumbnail

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,14 @@ import React from "react"
 import * as S from "./Card.styles"
 import ReactPlayer from "react-player/lazy"
 
+import ghs2 from "../../assets/ghs2.jpeg"
+
 export default ({className, name, videoSrc}) => {
     return (
         <S.Container>
             <S.Wrapper>
                 <S.Card className={className} >
-                    <ReactPlayer url={videoSrc} playing={true} controls width="100%" height="260px" light="https://www.billingsandedmonds.co.uk/bannerimg61.jpeg"/>
+                    <ReactPlayer url={videoSrc} playing={true} controls width="100%" height="260px" light={ghs2}/>
                     <S.Title>{name}</S.Title>
                 </S.Card>
             </S.Wrapper>
