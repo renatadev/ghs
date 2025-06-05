f95b2384f911a9551a81eaf7b27926bce6394127
Renata <41273801+renatadev@users.noreply.github.com>
2025-06-05 16:49:41 +0100

    feat(Cards): Remove Miss V singing

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -13,7 +13,6 @@ export default () => {
             <S.SingleCard videoSrc="https://youtu.be/_bYqIztF5v8" name="Piano - Mr Olly"/>
             <S.SingleCard videoSrc="https://youtu.be/_MKNEsz4zlc?si=P5xqnqhb41JoqA7A" name="Singing - Miss Antoinette"/>
             <S.SingleCard videoSrc="https://youtu.be/fwnNHBan8ww?si=XIqKgqiUcGc3NRP5" name="Singing - Miss Phoebe"/>
-            <S.SingleCard videoSrc="https://youtu.be/9pbRtbRkVao" name="Singing - Miss Vanessa"/>
             <S.SingleCard videoSrc="https://youtu.be/eqR94PIrNnY" name="Trumpet - Mr Paul"/>
             <S.SingleCard videoSrc="https://youtu.be/9pB-JESSnEI" name="Violin - Miss Diane"/>
             <S.SingleCard videoSrc="https://youtu.be/or8KYEOgbp8" name="Violin & Viola - Miss Joanne"/>
